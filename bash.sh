8a3f69ad7244b02908e835420ab1f64395fcf583
Emmanuel Christopher Oghre <emmyronald@gmail.com>
2023-08-12 20:33:15 +0100

    Changes

--- bash.sh
+++ bash.sh
@@ -1,85 +1,73 @@
 #!/bin/bash
 
-# Initialize an empty array to store records
-declare -a records
-
 # Function to add a new record
 add_record() {
-    echo "Adding a new record:"
+    echo "Enter new record details:"
     read -p "Name: " name
-    read -p "Phone Number: " phone
+    read -p "Age: " age
     read -p "Email: " email
 
-    records+=("$name,$phone,$email")
-    echo "Record added successfully!"
+    echo "$name|$age|$email" >> records.txt
+    echo "Record added successfully."
 }
 
 # Function to edit an existing record
 edit_record() {
-    echo "Editing an existing record:"
-    read -p "Enter the name of the record to edit: " search_name
+    read -p "Enter the name to edit: " search_name
 
-    for ((i = 0; i < ${#records[@]}; i++)); do
-        IFS=',' read -ra record <<< "${records[$i]}"
-        if [[ "${record[0]}" == "$search_name" ]]; then
-            read -p "New Phone Number: " new_phone
-            read -p "New Email: " new_email
+    # Search for the record
+    record=$(grep -i "^$search_name" records.txt)
 
-            records[$i]="${record[0]},$new_phone,$new_email"
-            echo "Record edited successfully!"
-            return
-        fi
-    done
+    if [ -z "$record" ]; then
+        echo "Record not found."
+    else
+        echo "Current record: $record"
+        read -p "Enter new age: " new_age
+        read -p "Enter new email: " new_email
 
-    echo "Record not found."
+        # Replace the existing record with the edited information
+        sed -i "s/^$search_name.*$/$search_name|$new_age|$new_email/" records.txt
+        echo "Record edited successfully."
+    fi
 }
 
 # Function to search for a specific record
 search_record() {
-    echo "Searching for a record:"
-    read -p "Enter the name to search for: " search_name
+    read -p "Enter the name to search: " search_name
 
-    for record in "${records[@]}"; do
-        IFS=',' read -ra fields <<< "$record"
-        if [[ "${fields[0]}" == "$search_name" ]]; then
-            echo "Name: ${fields[0]}"
-            echo "Phone Number: ${fields[1]}"
-            echo "Email: ${fields[2]}"
-            return
-        fi
-    done
+    # Search for the record
+    record=$(grep -i "^$search_name" records.txt)
 
-    echo "Record not found."
+    if [ -z "$record" ]; then
+        echo "Record not found."
+    else
+        echo "Record found: $record"
+    fi
 }
 
-# Function to generate a report of all records
+# Function to generate a report
 generate_report() {
-    echo "Generating Report:"
-    for record in "${records[@]}"; do
-        IFS=',' read -ra fields <<< "$record"
-        echo "Name: ${fields[0]}"
-        echo "Phone Number: ${fields[1]}"
-        echo "Email: ${fields[2]}"
-        echo "---------------------------"
-    done
+    echo "Generating report..."
+    cat records.txt
 }
 
 # Main menu
 while true; do
-    echo "Interactive Menu"
-    echo "1. Add New Record"
-    echo "2. Edit Existing Record"
-    echo "3. Search Specific Record"
+    echo "Menu:"
+    echo "1. Add Record"
+    echo "2. Edit Record"
+    echo "3. Search Record"
     echo "4. Generate Report"
     echo "5. Exit"
-    read -p "Select an option: " choice
-
+    
+    read -p "Enter your choice: " choice
+    
     case $choice in
-        1) add_record ;;
-        2) edit_record ;;
-        3) search_record ;;
-        4) generate_report ;;
-        5) echo "Goodbye!"; exit ;;
-        *) echo "Invalid choice. Please select again." ;;
+        1) add_record;;
+        2) edit_record;;
+        3) search_record;;
+        4) generate_report;;
+        5) exit;;
+        *) echo "Invalid choice. Please try again.";;
     esac
 done
